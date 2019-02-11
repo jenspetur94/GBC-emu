3e6285adda836813ed663c6c0a10fa40ac2c7a5a
unknown <jenspeturtrondarson@gmail.com>
2019-02-11 21:59:31 +0100

    CreatedProject

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GBC_emu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
